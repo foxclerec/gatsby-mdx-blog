7be95ab20f1ad9863a430cbba3c3317668124dae
foxclerec <anatolii.skoryk@gmail.com>
2021-12-25 20:17:05 +0100

    fixed typo

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import Theme from "./src/themes/themee";
+import Theme from "./src/themes/theme";
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={Theme}>{element}</ThemeProvider>
