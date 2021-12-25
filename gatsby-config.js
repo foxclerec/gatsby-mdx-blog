6219fd8e5c547510bff04a30f6cdeb799ee914d8
foxclerec <anatolii.skoryk@gmail.com>
2021-12-25 12:58:08 +0100

    added gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,27 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby-blog-skillthrive",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
